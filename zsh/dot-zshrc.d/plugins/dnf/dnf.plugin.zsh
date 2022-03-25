365e7722480474e1a8ec670f9021a79b3526047f
Chris Grau <113591+sirhc@users.noreply.github.com>
2022-03-24 21:25:47 -0700

    Add aliases for dnf

---
+++ zsh/dot-zshrc.d/plugins/dnf/dnf.plugin.zsh
@@ -0,0 +1,17 @@
+# vim: ft=zsh nospell
+
+if (( $+commands[dnf] )); then
+    alias dnfgl='dnf grouplist'      # list package groups
+    alias dnfl='dnf list'            # list packages
+    alias dnfli='dnf list installed' # list installed packages
+    alias dnfmc='dnf makecache'      # generate metadata cache
+    alias dnfp='dnf info'            # show package information
+    alias dnfs='dnf search'          # search package
+
+    alias dnfc='sudo dnf clean all'     # clean cache
+    alias dnfgi='sudo dnf groupinstall' # install package group
+    alias dnfgr='sudo dnf groupremove'  # remove package group
+    alias dnfi='sudo dnf install'       # install package
+    alias dnfr='sudo dnf remove'        # remove package
+    alias dnfu='sudo dnf upgrade'       # upgrade package
+fi
