04682871effe392410a5a99f663069dd8d9f448e
Chris Grau <113591+sirhc@users.noreply.github.com>
2022-11-22 08:34:25 -0800

    Add aliases for kb

---
+++ zsh/dot-zshrc.d/plugins/kb/kb.plugin.zsh
@@ -0,0 +1,9 @@
+if (( $+commands[kb] )); then
+    alias kbl='kb list'
+    alias kbe='kb edit'
+    alias kba='kb add'
+    alias kbv='kb view'
+    alias kbd='kb delete --id'
+    alias kbg='kb grep'
+    alias kbt='kb list --tags'
+fi
